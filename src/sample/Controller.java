d06053759f29df07d0eeaa2dea4ec5ab20fbf69a
Osman Residovic <you@example.com>
2017-06-28 21:17:15 +0200

    jednacine

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,259 +1,52 @@
 package sample;
-
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
-import jdk.internal.org.objectweb.asm.tree.analysis.Interpreter;
-
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import java.io.IOException;
+import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
-    public Button prefix;
+    public TextField one;
     @FXML
-    public Button zero;
-    @FXML
-    public Button backspace;
-    @FXML
-    public Button dot;
-    @FXML
-    public Button delete_all;
-    @FXML
-    public Button open_bracket;
-    @FXML
-    public Button close_bracket;
-    @FXML
-    public Button doResult;
-    @FXML
-    private Label result;
-    @FXML
-    private Button one;
-    @FXML
-    private Button two;
-    @FXML
-    private Button three;
-    @FXML
-    private Button four;
-    @FXML
-    private Button five;
-    @FXML
-    private Button six;
-    @FXML
-    private Button seven;
-    @FXML
-    private Button eight;
-    @FXML
-    private Button nine;
-    @FXML
-    private Button plus;
-    @FXML
-    private Button minus;
-    @FXML
-    private Button divide;
-    @FXML
-    private Button multiple;
-    @FXML
-    EventHandler handleNine = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "9");
-        }
-    };
-    EventHandler handleEight = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "8");
-        }
-    };
-    EventHandler handleSeven = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "7");
-        }
-    };
-    EventHandler handleSix = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "6");
-        }
-    };
-    EventHandler handleFive = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "5");
-        }
-    };
-    EventHandler handleFour = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "4");
-        }
-    };
-    EventHandler handleThree = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "3");
-        }
-    };
-    EventHandler handleTwo = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "2");
-        }
-    };
-    EventHandler handleOne = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "1");
-        }
-    };
-    EventHandler handleZero = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "0");
-        }
-    };
-    EventHandler handlePlus = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "+");
-        }
-    };
-    EventHandler handleMinus = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            if (checkCharacter()){
-                result.setText(result.getText() + "-");
-            }
-        }
-    };
-    EventHandler handleDivide = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            if (checkCharacter()) {
-                result.setText(result.getText() + "/");
-            }
-        }
-    };
-    EventHandler handleMultiple = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            if (checkCharacter()){
-                result.setText(result.getText() + "*");
-            }
+    public TextField two;
 
-        }
-    };
-    EventHandler handleBackspace = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText().substring(0,result.getText().length() -1));
-        }
-    };
-    EventHandler handleDeleteAll = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText("");
-        }
-    };
-    EventHandler handlePrefix = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "-");
-        }
-    };
-    EventHandler handleOpenBracket = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + "(");
-        }
-    };
-    EventHandler handleCloseBracket = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            result.setText(result.getText() + ")");
-        }
-    };
-    EventHandler handleDoResult = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-            ScriptEngineManager mgr = new ScriptEngineManager();
-            ScriptEngine engine = mgr.getEngineByName("JavaScript");
-            try {
-                result.setText(engine.eval(result.getText().toString()).toString());
-            } catch (ScriptException e) {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Error");
-                alert.setHeaderText("Problem with parsing!");
-                alert.setContentText("Expression is not valid!");
-                alert.showAndWait();
-            }
+    @FXML
+    public Label y;
+    @FXML
+    public Label x;
 
-        }
-    };
-    EventHandler handleDot = new EventHandler() {
+    @FXML
+    Button submit;
+
+    int x1= 4, y1 = -3, x2= 3, y2= -5, r1=-14, r2=-5;
+
+    EventHandler submitHandler = new EventHandler() {
         @Override
         public void handle(Event event) {
-            result.setText(result.getText() + ".");
+            x1 = Integer.parseInt(one.getText().substring(0, one.getText().indexOf('x')));
+            y1 = Integer.parseInt(one.getText().substring(one.getText().indexOf('x')+1, one.getText().indexOf('y')));
+            r1 = Integer.parseInt(one.getText().substring(one.getText().indexOf('=')+1, one.getText().length()));
+
+            x2 = Integer.parseInt(two.getText().substring(0, two.getText().indexOf('x')));
+            y2 = Integer.parseInt(two.getText().substring(two.getText().indexOf('x')+1, two.getText().indexOf('y')));
+            r2 = Integer.parseInt(two.getText().substring(two.getText().indexOf('=')+1, two.getText().length()));
+
+            double d = (x1*y2) -(y1*x2);
+            double dx = (r1*y2) -(r2*y1);
+            double dy = (r2*x1) - (r1*x2);
+            x.setText(String.valueOf(dx/d));
+            y.setText(String.valueOf(dy/d));
         }
     };
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        nine.setOnAction(handleNine);
-        eight.setOnAction(handleEight);
-        seven.setOnAction(handleSeven);
-        six.setOnAction(handleSix);
-        five.setOnAction(handleFive);
-        four.setOnAction(handleFour);
-        three.setOnAction(handleThree);
-        two.setOnAction(handleTwo);
-        one.setOnAction(handleOne);
-        zero.setOnAction(handleZero);
-        plus.setOnAction(handlePlus);
-        minus.setOnAction(handleMinus);
-        multiple.setOnAction(handleMultiple);
-        divide.setOnAction(handleDivide);
-        backspace.setOnAction(handleBackspace);
-        delete_all.setOnAction(handleDeleteAll);
-        prefix.setOnAction(handlePrefix);
-        open_bracket.setOnAction(handleOpenBracket);
-        close_bracket.setOnAction(handleCloseBracket);
-        doResult.setOnAction(handleDoResult);
-        dot.setOnAction(handleDot);
+        submit.setOnAction(submitHandler);
     }
-
-    private boolean checkCharacter(){
-        if (
-                result.getText().length() > 1 && (result.getText().charAt(result.getText().length() -1) == '0' ||
-                        result.getText().charAt(result.getText().length() -1) == '1' ||
-                        result.getText().charAt(result.getText().length() -1) == '2')){
-            return true;
-        }
-        return false;
-    }
-
-    /*
-     *  Platform.runLater(new Runnable() {
-                public void run() {
-                    try {
-                        new Screen().start(new Stage());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }); */
 }
